4a49409b263343849bac5f9b7fc0632c407aea72
yangxinyu <742707786@qq.com>
2024-05-20 09:30:08 +0800

    feat:change python311 to python39

--- src/sql/engine/expr/ob_expr_python_udf.cpp
+++ src/sql/engine/expr/ob_expr_python_udf.cpp
@@ -1041,8 +1041,8 @@ int ObExprPythonUdf::eval_test_udf_batch(const ObExpr &expr, ObEvalCtx &ctx,
   // }
   // Py_XDECREF(resultarray);
 
-  PyGC_Enable();
-  PyGC_Collect();
+  // PyGC_Enable();
+  // PyGC_Collect();
   
   //release GIL
   if(nStatus)
