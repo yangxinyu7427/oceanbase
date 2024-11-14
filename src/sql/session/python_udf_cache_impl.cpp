70adea8a94f4cb008dd9a932f729003de1ab436f
yxy <742707786@qq.com>
2024-11-14 17:19:30 +0800

    feat: finish get method of UDF cache

--- src/sql/session/python_udf_cache_impl.cpp
+++ src/sql/session/python_udf_cache_impl.cpp
@@ -126,6 +126,64 @@ namespace sql{
             return ret;
         }
 
+        int get_int(const common::ObString& udf_name, char* key, int& value){
+            int ret = OB_SUCCESS;
+            PyUdfCacheMapForInt *cache_map;
+            // 判断udf是否已建立相关缓存，如果没建立，就去初始化一个缓存
+            std::string udf_str(udf_name.ptr(),udf_name.length());
+            if(udf_list.find(udf_str)==udf_list.end()){
+                cache_map=new PyUdfCacheMapForInt();
+                if(OB_FAIL(cache_map->create(hash::cal_next_prime(500000),
+                                              ObModIds::OB_HASH_BUCKET,
+                                              ObModIds::OB_HASH_NODE))){
+                    LOG_WARN("new_cache_map create fail", K(ret));
+                }else if(OB_FAIL(cache_map_for_int_.set_refactored(udf_name, cache_map))){
+                    LOG_WARN("cache_map_for_int set fail", K(ret));
+                }
+                udf_list.insert(udf_str);
+            }else{
+                // 有缓存就直接拿出来
+                if(OB_FAIL(cache_map_for_int_.get_refactored(udf_name, cache_map))){
+                    LOG_WARN("cache_map_for_int get fail", K(ret));
+                }
+            }
+            // 将数据从map中取出
+            if(OB_FAIL(cache_map->get_refactored(key, value))){
+                LOG_WARN("cache_map set fail", K(ret));
+            }
+            return ret;
+        }
+
+        int get_string(const common::ObString& udf_name, char* key, string& value){
+            int ret = OB_SUCCESS;
+            PyUdfCacheMapForString *cache_map;
+            // 判断udf是否已建立相关缓存，如果没建立，就去初始化一个缓存
+            std::string udf_str(udf_name.ptr(),udf_name.length());
+            if(udf_list.find(udf_str)==udf_list.end()){
+                cache_map=new PyUdfCacheMapForString();
+                if(OB_FAIL(cache_map->create(hash::cal_next_prime(500000),
+                                              ObModIds::OB_HASH_BUCKET,
+                                              ObModIds::OB_HASH_NODE))){
+                    LOG_WARN("new_cache_map create fail", K(ret));
+                }else if(OB_FAIL(cache_map_for_string_.set_refactored(udf_name, cache_map))){
+                    LOG_WARN("cache_map_for_string set fail", K(ret));
+                }
+                udf_list.insert(udf_str);
+            }else{
+                // 有缓存就直接拿出来
+                if(OB_FAIL(cache_map_for_string_.get_refactored(udf_name, cache_map))){
+                    LOG_WARN("cache_map_for_string get fail", K(ret));
+                }
+            }
+            // 将数据存入map
+            std::shared_ptr<std::string> value_ptr;
+            if(OB_FAIL(cache_map->get_refactored(key, value_ptr))){
+                LOG_WARN("cache_map set fail", K(ret));
+            }
+            value=*value_ptr.get();
+            return ret;
+        }
+
     };
 
 }
